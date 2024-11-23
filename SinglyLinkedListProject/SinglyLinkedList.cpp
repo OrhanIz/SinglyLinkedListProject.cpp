cf8bb3843dc68382a11b2308c4a8409f788b147f
OrhanIz <izmiro4914730_aehit@students.vizja.pl>
2024-11-23 17:30:23 +0100

    SinglyLinkedListProject

---
+++ SinglyLinkedListProject/SinglyLinkedList.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+
+template <typename T>
+class SinglyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T value) : data(value), next(nullptr) {}
+    };
+    Node* head;
+
+public:
+    SinglyLinkedList() : head(nullptr) {}
+
+    void addFirst(T value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void addLast(T value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void removeFirst() {
+        if (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void removeLast() {
+        if (head == nullptr) return;
+        if (head->next == nullptr) {
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != nullptr && temp->next->next != nullptr) {
+            temp = temp->next;
+        }
+        delete temp->next;
+        temp->next = nullptr;
+    }
+
+    int size() {
+        int count = 0;
+        Node* temp = head;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+
+    Node* find(T value) {
+        Node* current = head;
+        while (current != nullptr) {
+            if (current->data == value) {
+                return current;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    void printList() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << "->";
+            temp = temp->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    SinglyLinkedList<int> list;
+
+    list.addFirst(10);
+    list.addFirst(20);
+    list.addLast(5);
+    list.addFirst(30);
+
+    std::cout << "List after additions: ";
+    list.printList();
+
+    list.removeFirst();
+    std::cout << "List after removing first: ";
+    list.printList();
+
+    list.removeLast();
+    std::cout << "List after removing last: ";
+    list.printList();
+
+    std::cout << "Size of the list: " << list.size() << std::endl;
+
+    std::cout << "Enter the number to find: ";
+    int numberToFind;
+    std::cin >> numberToFind;
+
+    auto result = list.find(numberToFind);
+    if (result != nullptr) {
+        std::cout << "Found node with value: " << result->data << std::endl;
+    } else {
+        std::cout << "Value not found in the list." << std::endl;
+    }
+
+    return 0;
+}
---
+++ SinglyLinkedListProject/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "SinglyLinkedList.cpp"
+
+int main() {
+    SinglyLinkedList<int> l1;
+    l1.addATFront(5);
+    l1.addATFront(10);
+    l1.addAtEnd(20);
+    l1.addATFront(30);
+
+    std::cout << "List after additions: ";
+    l1.displayList();
+
+    l1.removeFirst();
+    std::cout << "List after removing first: ";
+    l1.displayList();
+
+    l1.removeLast();
+    std::cout << "List after removing last: ";
+    l1.displayList();
+
+    std::cout << "Size of the list: " << l1.size() << std::endl;
+
+    int valueToFind;
+    std::cout << "Enter the number to find: ";
+    std::cin >> valueToFind;
+
+    Node<int>* foundNode = l1.find(valueToFind);
+    if (foundNode != nullptr) {
+        std::cout << "Found value " << valueToFind << " in the list." << std::endl;
+    }
+    else {
+        std::cout << "Value " << valueToFind << " not found in the list." << std::endl;
+    }
+
+    return 0;
+}
